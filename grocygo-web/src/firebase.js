d4bcb7b4202653fe574c4aed4fe72c3961c01987
prit1520 <jambaroverseas@gmail.com>
2025-07-27 15:09:38 +0530

    add security

--- grocygo-web/src/firebase.js
+++ grocygo-web/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwZQDYYbWHPnvHHPmCqLcv3gu7qeEb-QQ",
-  authDomain: "grocygo-c9820.firebaseapp.com",
-  projectId: "grocygo-c9820",
-  storageBucket: "grocygo-c9820.appspot.com",
-  messagingSenderId: "90041522201",
-  appId: "1:90041522201:web:56f663babafa7b8973b288",
-  measurementId: "G-0LNT8ZGD4P"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
